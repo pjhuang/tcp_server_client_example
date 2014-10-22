69cdc566af71937697f9276f7abd61198bc4f4c6
pjhuang <pjhuang+github@gmail.com>
2014-10-22 16:52:20 +0800

    1. revise tcpclient.c for using select() for read/write 2. add timer mechanism

--- tcpclient.c
+++ tcpclient.c
@@ -18,6 +18,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 #include "common.h"
 
@@ -36,10 +37,19 @@
 *****************************************************************************/
 #define TCP_PORT            503
 #define TX_RX_BUFFER_SIZE   32
+#define TX_INTERVAL_SEC     1       /* sec */
+#define TX_INTERVAL_USEC    0       /* usec */
+#define DIS_INTERVAL        2
 
 /*****************************************************************************
 ** Type definitions
 *****************************************************************************/
+typedef enum
+{
+    TIMER_TX,
+    TIMER_DIS,
+    TIMER_MAX_NUM
+} tc_timer_t;
 
 
 /*****************************************************************************
@@ -58,11 +68,28 @@ do                      \
 ** Local variables
 *****************************************************************************/
 static pthread_t    tClientThread;
+static pthread_t    timerThread;
+
 static char dstIpStr[20] = {0};
 int sockfd;
 struct sockaddr_in servaddr;
 struct sockaddr_in cliaddr;
+static struct timeval time_now;
+static const struct timeval tx_ti = {TX_INTERVAL_SEC, TX_INTERVAL_USEC};
+static struct timeval tx_to;
 
+static const struct timeval dis_ti = {DIS_INTERVAL, 0};
+static struct timeval dis_to;
+static int timeToTx = 0;
+static int timeToDis = 0;
+
+static char *unknownStr = "Uknown type";
+static char *tcTimerTypeStr[] =
+{
+    "TIMER_TX",
+    "TIMER_DIS",
+    "TIMER_UNKNOWN"
+};
 
 
 /*****************************************************************************
@@ -78,6 +105,79 @@ struct sockaddr_in cliaddr;
 /*****************************************************************************
 ** Function bodies
 *****************************************************************************/
+static char *convertTimerToStr(tc_timer_t timerType)
+{
+    if (timerType >= TIMER_MAX_NUM)
+    {
+        return unknownStr;
+    }
+    
+    return tcTimerTypeStr[timerType];
+}
+
+
+static void timerStart(tc_timer_t timerType)
+{
+    PRINTF_DBG(DEBUG_LEVEL, "%s\n", convertTimerToStr(timerType));
+
+    switch (timerType)
+    {
+        case TIMER_TX:
+            timeradd(&time_now, &tx_ti, &tx_to);
+            break;
+
+        case TIMER_DIS:
+            timeradd(&time_now, &dis_ti, &dis_to);
+            break;
+            
+        default:
+            ASSERT(0, "Unknow timer");
+            break;
+    }
+}
+
+
+#if 0
+static void timerStop(tc_timer_t timerType)
+{
+    PRINTF_DBG(DEBUG_LEVEL, "%s\n", convertTimerToStr(timerType));
+
+    switch (timerType)
+    {
+        case TIMER_TX:
+            timerclear(&tx_to);
+            break;
+
+        case TIMER_DIS:
+            timerclear(&dis_to);
+            break;
+
+        default:
+            ASSERT(0, "Unknow timer");
+            break;
+    }
+}
+#endif
+
+
+static void timerCheck(void)
+{
+    /* TIMER_TX */
+    if (timerisset(&tx_to) && timercmp(&time_now, &tx_to, >=))
+    {
+        timerclear(&tx_to);
+        timerStart(TIMER_TX);
+        timeToTx = 1;
+    }
+
+    /* TIMER_DIS */
+    if (timerisset(&dis_to) && timercmp(&time_now, &dis_to, >=))
+    {
+        timerclear(&dis_to);
+        timerStart(TIMER_DIS);
+        timeToDis = 1;
+    }
+}
 
 
 static int tclient_init(void)
@@ -109,70 +209,98 @@ static int tclient_init(void)
 }
 
 
-static int tclient_loop(void)
+static int tclient_tx(void)
 {
-#if 1
+    static int i = 0;
+    int writenum;
+    char txData[TX_RX_BUFFER_SIZE] = {0};
+
+    if (timeToTx)
+    {
+        timeToTx = 0;
+   
+        sprintf(txData, "This is a book, %d", i++);
+        writenum = write(sockfd, txData, strlen(txData));
+        if (writenum != strlen(txData))
+        {
+            PRINTF_DBG(DEBUG_LEVEL, "%s\n", strerror(errno));
+            return -1;
+        }
+        
+        PRINTF_DBG(DEBUG_LEVEL, "writenum = %d\n", writenum);
+    }
+
+    return 0;
+}
+
+
+static int tclient_rx(void)
+{
+    int readnum;
+    char rxData[TX_RX_BUFFER_SIZE] = {0};
+
+    readnum = recvfrom(sockfd, rxData, TX_RX_BUFFER_SIZE, 0, NULL, NULL);
+    PRINTF_DBG(DEBUG_LEVEL, "readnum = %d, %s\n", readnum, rxData);
+    if (readnum == 0) 
+    {
+        PRINTF_DBG(DEBUG_LEVEL, "client disconnected\n");
+        return (-1);
+    }
+
+    return 0;
+}
+
+
+static int tclient_select(void)
+{
+    int ret;
+    fd_set readfs;
+    fd_set writefs;
+    struct timeval to;
+
     if (0 >= sockfd)
     {
         return 0;
     }
 
-    while(1)
+    FD_ZERO(&readfs); 
+    FD_SET(sockfd, &readfs);
+    FD_ZERO(&writefs); 
+    FD_SET(sockfd, &writefs);
+
+    to.tv_sec = 1;
+    to.tv_usec = 0;
+
+    ret = select(sockfd + 1, &readfs, &writefs, NULL, &to);
+    //PRINTF_DBG(RUNNING_LEVEL, "select return %d\n", ret);
+    if (ret < 0)
     {
-        int ret;
-        static i = 0;
-        int writenum;
-        int readnum;
-        char txData[TX_RX_BUFFER_SIZE] = {0};
-        char rxData[TX_RX_BUFFER_SIZE] = {0};
-
-        sprintf(txData, "This is a book, %d", i++);
-        writenum = write(sockfd, txData, strlen(txData));
-        if (writenum != strlen(txData))
+        PRINTF_DBG(RUNNING_LEVEL, "TCP client thread created, thread ID[%lu]\n", pthread_self());
+    }
+    else if (ret == 0)
+    {
+        PRINTF_DBG(RUNNING_LEVEL, "select timeout\n");
+    }
+    else
+    {
+        if (FD_ISSET(sockfd, &readfs))
         {
-            PRINTF_DBG(DEBUG_LEVEL, "ret = %d, %s\n", ret, strerror(errno));
+            if (-1 == tclient_rx())
+            {
+                return (-1);
+            }
         }
 
-        PRINTF_DBG(DEBUG_LEVEL, "writenum = %d\n", writenum);
-#if 1
-        readnum = recvfrom(sockfd, rxData, TX_RX_BUFFER_SIZE, 0, NULL, NULL);
-        PRINTF_DBG(DEBUG_LEVEL, "readnum = %d, %s\n", readnum, rxData);
-        if (readnum == 0) 
+        if (FD_ISSET(sockfd, &writefs))
         {
-            PRINTF_DBG(DEBUG_LEVEL, "client disconnected\n");
-            return (-1);
+            if (-1 == tclient_tx())
+            {
+                return (-1);
+            }
         }
-#else
-        readnum = read(sockfd, rxData, TX_RX_BUFFER_SIZE);
-        PRINTF_DBG(DEBUG_LEVEL, "readnum = %d, %s\n", readnum, rxData);
-        if (0 < readnum)
-        {
-            PRINTF_DBG(DEBUG_LEVEL, "received data: %s\n", rxData);
-        }
-#endif
-
-        sleep(1);
-        //usleep(1000);
-
-        return -1;
-   }
+    }
 
    return 0;
-#else
-        while (fgets(sendline, 10000, stdin) != NULL)
-        {
-            int n;   
-            char sendline[1000];
-            char recvline[1000];
-    
-            sendto(sockfd, sendline, strlen(sendline), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
-    
-            n = recvfrom(sockfd, recvline, 10000, 0, NULL, NULL);
-            recvline[n] = 0;
-            fputs(recvline,stdout);
-       }
-#endif
-
 }
 
 
@@ -191,16 +319,40 @@ static void *tclient_thread(void * arg)
             {
                 cLOSEsOCKET(sockfd);
             }
+            else
+            {
+                timerStart(TIMER_TX);
+                timerStart(TIMER_DIS);
+            }
         }
 
-        ret = tclient_loop();
+        ret = tclient_select();
         if (-1 == ret)
         {
             cLOSEsOCKET(sockfd);
         }
+        else if (timeToDis)
+        {
+            cLOSEsOCKET(sockfd);
+            timeToDis = 0;
+        }
     }
+
+    return NULL;
 }
 
+
+static void *timer_thread(void * arg) 
+{
+    while (1)
+    {
+        gettimeofday(&time_now, NULL);
+
+        timerCheck();
+    }
+
+    return NULL;
+}
 
 
 int main(int argc, char**argv)
@@ -233,6 +385,13 @@ int main(int argc, char**argv)
        exit(1);
    }
 
+   ret = pthread_create(&timerThread, NULL, timer_thread, NULL);
+   if (ret != 0) 
+   {
+       PRINTF_DBG(ERROR_LEVEL, "Create tClientThread failed, %s\n", strerror(errno));
+       exit(1);
+   }
+
     ret = pthread_join(tClientThread, (void *)NULL);
     if (ret != 0) 
     {
@@ -242,7 +401,6 @@ int main(int argc, char**argv)
         exit(1);
     }   
 
-
-
+    return 0;
 }
 
